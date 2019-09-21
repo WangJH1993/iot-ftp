a3725b8f190e4d98a0c7272e99a9cf3e876af012
WangJH1993 <916023386@qq.com>
2019-09-21 20:30:35 +0800

    'test111'

--- src/main/java/com/sitech/iotftp/IotFtpApplication.java
+++ src/main/java/com/sitech/iotftp/IotFtpApplication.java
@@ -26,6 +26,7 @@ public class IotFtpApplication {
         sub.receive();
         System.out.println("111111111111111111111111");
         System.out.println("111111111111111111111111");
+        System.out.println("111111111111111111111s111");
     }
 
 }
